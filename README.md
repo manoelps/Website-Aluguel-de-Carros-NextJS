9c75e24455ebc50a436f10bbe2f71101788b38ef
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-17 08:02:52 -0300

    criado a estrutura inicial do projeto

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Website-Aluguel-de-Carros-NextJS
-Site para aluguel de automóveis criado com Next.JS v14.2.3
+
+Site para aluguel de automóveis criado com Next.JS v14.2.3, utilizando [Next.js](https://nextjs.org/)
+
+## Getting Started
+
+Primeiro, execute o servidor de desenvolvimento:
+
+```bash
+npm run dev
+# ou
+yarn dev
+# ou
+pnpm dev
+# ou
+bun dev
+```
+
+Abra [http://localhost:3000](http://localhost:3000) com seu navegador para ver o resultado.
+
+##
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Site para aluguel de automóveis criado com Next.JS v14.2.3',
+  description: 'Site para aluguel de automóveis criado com Next.JS v14.2.3'
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
