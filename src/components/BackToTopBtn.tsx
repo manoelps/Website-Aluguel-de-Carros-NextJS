9fd147d1fc20a9e3867ebdcafde659e5b23e53ac
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-18 16:24:13 -0300

    criado componente  BackToTopBtn

--- src/components/BackToTopBtn.tsx
+++ src/components/BackToTopBtn.tsx
@@ -1,5 +1,38 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { FaChevronUp } from 'react-icons/fa6';
+import { Link } from 'react-scroll';
+
 const BackToTopBtn = () => {
-  return <div>BackToTopBtn</div>;
+  const [isActive, setActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 400 ? setActive(true) : setActive(false);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // clean up the event listeners
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+  return (
+    <Link
+      to="home"
+      smooth
+      spy
+      className={`${
+        !isActive && 'hidden'
+      } fixed bg-accent hover:bg-accent-hover w-12 h-12 right-16 bottom-11 z-10 cursor-pointer
+      flex justify-center items-center text-white border-2 border-white`}
+    >
+      <FaChevronUp className="text-xl" />
+    </Link>
+  );
 };
 
 export default BackToTopBtn;
